108cb5cc8d8125c715bb77f0d86625c75374ba8f
Serhii Bordeniuk <brdnk.srh@gmail.com>
2024-01-17 15:19:53 +0200

    event details page, event card and events list added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["utfs.io"],
+        remotePatterns: [{ protocol: "https", hostname: "utfs.io" }],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
